b4234e886db7a441c63e783709b69f5f2270dfa9
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-03-02 11:17:09 -0600

    added some java

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hi');
+// console.log('hi');
+
+// //Jeressia Williamson
+// //Beyonce Knowles-Carter
+// //Tori Kelly
+// //Sam Smith
+// //Jane Doe
+
+const firstName='Jeressia'
+const lastName='Williamson'
+
+// console.log(`${firstName} ${lastName}`);
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+    alert(`${firstName} ${lastName}`)
+};
+
+namePrinter('Jeressia','Williamson');
+namePrinter('Beyonce','Knowles-Carter');
+namePrinter('Tori','Kelly');
+namePrinter('Sam','Smith')
+namePrinter('Jane','Doe')
