bd6bbdd6f5cfa039a64794be541b9c343f0f3552
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-03-02 13:46:46 -0600

    updated javascript

--- main.js
+++ main.js
@@ -22,7 +22,33 @@ namePrinter('Sam','Smith')
 namePrinter('Jane','Doe')
 
 const nuggetizer = (animal) => {
-    return `processed ${animal}`;
+    return `Processed ${animal}`;
 };
 
 console.log(nuggetizer('monkey'));
+
+const dogBreed=(dogBreed) => {
+    return `My favorite dog breed is ${dogBreed}`
+}
+
+const dogBreedDiv = document.getElementById('dog-breeds');
+
+// console.log('dogBreedDiv', dogBreedDiv)
+// dogBreedDiv.innerHTML = dogBreed('lab');
+
+// const nuggetizerDiv = document.getElementById('nuggetizer');
+
+// console.log('nuggetizerDiv', nuggetizerDiv)
+// nuggetizerDiv.innerHTML= nuggetizer('Salmon');
+// nuggetizerDiv.innerHTML += nuggetizer('mouse')
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv= document.getElementById(divId)
+    selectedDiv.innerHTML += textToPrint;
+};
+
+printToDom('dog-breeds', dogBreed('bear'));
+printToDom('dog-breeds', dogBreed('kitten'));
+printToDom('nuggetizer',nuggetizer('kitten'));
+printToDom('nuggetizer','mmmmmmmmmm');
+
